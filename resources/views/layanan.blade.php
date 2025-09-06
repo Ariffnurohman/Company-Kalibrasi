cfd90e603dd5af50c69ff8eb35b3ded95f6870ba
Arifnurohman <if23.arifnurohman@mhs.ubpkarawang.ac.id>
2025-09-06 17:16:17 +0700

    footer

--- resources/views/layanan.blade.php
+++ resources/views/layanan.blade.php
@@ -58,3 +58,50 @@
   </div>
 </section>
 
+<!-- Footer Section -->
+<footer class="bg-dark text-light pt-5 pb-4">
+  <div class="container">
+    <div class="row">
+
+      <!-- Recent Post -->
+      <div class="col-md-4 mb-4">
+        <h5 class="footer-heading">Recent Post</h5>
+        <ul class="list-unstyled">
+          <li><a href="#" class="text-light text-decoration-none">Pelatihan Kalibrasi Alat Ukur</a></li>
+          <li><a href="#" class="text-light text-decoration-none">Workshop Instrumental Kimia</a></li>
+          <li><a href="#" class="text-light text-decoration-none">Sertifikasi Peserta</a></li>
+        </ul>
+        <img src="{{ asset('images/kan logo.png') }}" class="img-fluid rounded shadow mt-1" alt="sertifikasi kan">
+
+      </div>
+
+      <!-- Kontak Kami -->
+      <div class="col-md-4 mb-4">
+        <h5 class="footer-heading">Kontak Kami</h5>
+        <p>Email: info@perusahaan.com</p>
+        <p>Telepon: +62 812-3456-7890</p>
+        <p>Website: www.perusahaan.com</p>
+      </div>
+
+      <!-- Alamat & Lokasi -->
+      <div class="col-md-4 mb-4">
+        <h5 class="footer-heading">Alamat & Lokasi</h5>
+        <p>JL. RAYA TELUKJAMBE N0.8 KSB RAYA 8</p>
+        <p>KARAWANG SENTRA BIZHUB, KARAWANG</p>
+        <div class="map-container mt-2">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18..."
+            width="100%" height="200" style="border:0;" allowfullscreen loading="lazy">
+          </iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-center mt-4">
+      <p class="mb-0">&copy; {{ date('Y') }} PT Rukun Sejahtera Teknik. All rights reserved.</p>
+    </div>
+  </div>
+</footer>
+
+
+
